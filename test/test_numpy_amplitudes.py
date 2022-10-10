f70a193a50f1d008a23af39484b9334115ea77e4
Logan A. Morrison <loganmorrison99@gmail.com>
2022-10-09 18:32:25 -0700

    [test] Adding more tests
    Adding additional tests to target more internal functionality

--- test/test_numpy_amplitudes.py
+++ test/test_numpy_amplitudes.py
@@ -13,12 +13,14 @@ MUON_MASS = 1.056584e-01
 G_FERMI = 1.166379e-05
 EL = 3.028620e-01
 SW = 4.808530e-01
+CW = np.sqrt(1.0 - SW**2)
 MASS_W = 8.038500e01
 WIDTH_W = 2.085000e00
 MASS_B = 4.18
 MASS_T = 172.9
 MASS_MU = 105.6583745e-3
 MASS_E = 0.5109989461e-3
+MASS_Z = 91.1876
 
 MASS_H = 125.00
 WIDTH_H = 0.00374
@@ -191,11 +193,9 @@ def test_width_t_to_b_w():
     analytic = (
         EL**2
         * (
-            MASS_B**4
-            + MASS_T**4
-            + MASS_T**2 * MASS_W**2
-            - 2 * MASS_W**4
-            + MASS_B**2 * (-2 * MASS_T**2 + MASS_W**2)
+            (MASS_T**2 + 2 * MASS_W**2) * (MASS_T**2 - MASS_W**2)
+            + MASS_B**2 * (MASS_W**2 - 2 * MASS_T**2)
+            + MASS_B**4
         )
         * np.sqrt(kallen_lambda(MASS_B**2, MASS_T**2, MASS_W**2))
     ) / (64 * MASS_T**3 * MASS_W**2 * np.pi * SW**2)
@@ -206,41 +206,52 @@ def test_width_t_to_b_w():
     assert float(width) == pytest.approx(analytic, rel=1e-3, abs=0.0)
 
 
-def test_width_h_to_b_b():
-    def msqrd_h_to_b_b(momenta):
+# ============================================================================
+# ---- H -> f + f ------------------------------------------------------------
+# ============================================================================
+
+
+def widths_h_to_f_f(mf, ncf):
+    yf = mf / VEV_H
+    vertex = amplitudes.VertexFFS(
+        left=yf / np.sqrt(2),
+        right=yf / np.sqrt(2),
+    )
+
+    def msqrd_h_to_f_f(momenta):
         p1 = momenta[:, 0]
         p2 = momenta[:, 1]
         ph = np.sum(momenta, axis=1)
 
-        wf1s = spinor_ubar(p1, MASS_B)
-        wf2s = spinor_v(p2, MASS_B)
+        wf1s = spinor_ubar(p1, mf)
+        wf2s = spinor_v(p2, mf)
         h_wf = scalar_wf(ph, False)
 
-        v_hbb = amplitudes.VertexFFS(
-            left=EL * MASS_B / (2 * MASS_W * SW), right=EL * MASS_B / (2 * MASS_W * SW)
-        )
-
         def msqrd(i1, i2):
-            return abs2(amplitude_ffs(v_hbb, wf1s[i2], wf2s[i1], h_wf))
+            return abs2(amplitude_ffs(vertex, wf1s[i2], wf2s[i1], h_wf))
 
         idxs = np.array(np.meshgrid([0, 1], [0, 1])).T.reshape(-1, 2)
         res = sum(msqrd(i1, i2) for (i1, i2) in idxs)
 
-        return res
+        return ncf * res
 
-    mub = MASS_B / MASS_H
-    analytic = (
-        EL**2
-        * MASS_H**3
-        * mub**2
-        * (1 - 4 * mub**2) ** 1.5
-        / (32 * MASS_W**2 * np.pi * SW**2)
-    )
+    mu = mf / MASS_H
+    analytic = ncf * yf**2 * MASS_H * (1 - 4 * mu**2) ** 1.5 / (16 * np.pi)
 
-    phase_space = PhaseSpace(MASS_H, [MASS_B, MASS_B], msqrd=msqrd_h_to_b_b)
+    phase_space = PhaseSpace(MASS_H, [mf, mf], msqrd=msqrd_h_to_f_f)
     width = phase_space.decay_width(1000, seed=SEED)[0]
 
+    return width, analytic
+
+
+def test_width_h_to_b_b():
+    width, analytic = widths_h_to_f_f(mf=MASS_B, ncf=3.0)
     assert float(width) == pytest.approx(analytic, rel=1e-3, abs=0.0)
+
+
+# ============================================================================
+# ---- chi + chi -> S -> f + f -----------------------------------------------
+# ============================================================================
 
 
 def test_dark_matter_annihilation_scalar_mediator():
@@ -291,6 +302,11 @@ def test_dark_matter_annihilation_scalar_mediator():
     ) / (16 * np.pi * s * ((s - mass_s**2) ** 2 + (mass_s * width_s) ** 2))
 
     assert float(cs) == pytest.approx(analytic, rel=1e-2, abs=0.0)
+
+
+# ============================================================================
+# ---- chi + chi -> V -> f + f -----------------------------------------------
+# ============================================================================
 
 
 def test_dark_matter_annihilation_vector_mediator():
@@ -348,3 +364,130 @@ def test_dark_matter_annihilation_vector_mediator():
     )
 
     assert float(cs) == pytest.approx(analytic, rel=1e-2, abs=0.0)
+
+
+# ============================================================================
+# ---- Z -> f + f ------------------------------------------------------------
+# ============================================================================
+
+
+def widths_z_to_f_f(mf, ncf, t3f, qf):
+    pre = EL / (SW * CW)
+    left = pre * (t3f - qf * SW**2)
+    right = -pre * qf * SW**2
+    vertex = amplitudes.VertexFFV(left=left, right=right)
+
+    def msqrd_z_to_f_f(momenta):
+        pf1 = momenta[:, 0]
+        pf2 = momenta[:, 1]
+        pz = np.sum(momenta, axis=1)
+
+        z_wfs = polvec(pz, MASS_Z, False)
+        ubar = spinor_ubar(pf1, mf)
+        v = spinor_v(pf2, mf)
+
+        def msqrd(iz, i1, i2):
+            amp = amplitude_ffv(vertex, ubar[i1], v[i2], z_wfs[iz])
+            return abs2(amp)
+
+        idxs = np.array(np.meshgrid([0, 1, 2], [0, 1], [0, 1])).T.reshape(-1, 3)
+        res = sum(msqrd(iz, i1, i2) for (iz, i1, i2) in idxs)
+
+        return ncf * res / 3.0
+
+    mr2 = (mf / MASS_Z) ** 2
+    af = t3f - qf * SW**2
+    bf = -qf * SW**2
+
+    analytic = (
+        ncf
+        * EL**2
+        * MASS_Z
+        / (24 * np.pi * CW**2 * SW**2)
+        * np.sqrt(1 - 4 * mr2)
+        * ((af**2 + bf**2) * (1 - mr2) + 6 * af * bf * mr2)
+    )
+
+    phase_space = PhaseSpace(MASS_Z, [mf, mf], msqrd=msqrd_z_to_f_f)
+    width = phase_space.decay_width(10_000, seed=SEED)[0]
+
+    return width, analytic
+
+
+def test_width_z_to_b_b():
+    mf = MASS_B
+    ncf = 3
+    t3f = -0.5
+    qf = -1.0 / 3.0
+
+    width, analytic = widths_z_to_f_f(mf=mf, ncf=ncf, t3f=t3f, qf=qf)
+    assert float(width / analytic) == pytest.approx(1, rel=1e-3, abs=0.0)
+
+
+def test_width_z_to_e_e():
+    mf = MASS_E
+    ncf = 1
+    t3f = -0.5
+    qf = -1.0
+
+    width, analytic = widths_z_to_f_f(mf=mf, ncf=ncf, t3f=t3f, qf=qf)
+    assert float(width / analytic) == pytest.approx(1.0, rel=1e-3, abs=0.0)
+
+
+# ============================================================================
+# ---- Ni  -> Nj + phi -------------------------------------------------------
+# ============================================================================
+
+
+def widths_ni_to_nj_phi(mi, mj, mphi, y):
+
+    vertex = amplitudes.VertexFFS(left=y, right=y)
+
+    def msqrd_ni_to_nj_phi(momenta):
+        pj = momenta[:, 0]
+        pphi = momenta[:, 1]
+        pi = np.sum(momenta, axis=1)
+
+        wfi_u = spinor_u(pi, mi)
+        wfi_vbar = spinor_vbar(pi, mi)
+
+        wfj_ubar = spinor_ubar(pj, mj)
+        wfj_v = spinor_v(pj, mj)
+
+        wfphi = scalar_wf(pphi, mphi)
+
+        def msqrd(ii, ij):
+            amp = amplitude_ffs(vertex, wfj_ubar[ij], wfi_u[ii], wfphi) - amplitude_ffs(
+                vertex, wfi_vbar[ii], wfj_v[ij], wfphi
+            )
+            return abs2(amp)
+
+        idxs = np.array(np.meshgrid([0, 1], [0, 1])).T.reshape(-1, 2)
+        res = sum(msqrd(ii, ij) for (ii, ij) in idxs)
+
+        return res / 2.0
+
+    rj = mj**2 / mi**2
+    rphi = mphi**2 / mi**2
+
+    analytic = (
+        mi
+        / (16 * np.pi)
+        * np.sqrt(kallen_lambda(1, rphi, rj))
+        * (np.abs(y) ** 2 * (1 + rj - rphi) + 2 * np.sqrt(rj) * np.real(y**2))
+    )
+
+    phase_space = PhaseSpace(mi, [mj, mphi], msqrd=msqrd_ni_to_nj_phi)
+    width = phase_space.decay_width(10_000, seed=SEED)[0]
+
+    return width, analytic
+
+
+def test_width_ni_to_nj_phi():
+    mi = 100.0
+    mj = 25.0
+    mphi = 3.0
+    y = 1.0
+
+    width, analytic = widths_ni_to_nj_phi(mi=mi, mj=mj, mphi=mphi, y=y)
+    assert float(width / analytic) == pytest.approx(1, rel=1e-3, abs=0.0)
